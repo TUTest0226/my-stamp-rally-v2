f132114a0041906f2fa43cad19fd2161aeffab95
TU <modptu@gmail.com>
2025-06-25 11:43:24 +0900

    dynamodb.tsのcredentialに関する記述修正

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -3,15 +3,19 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const config: DynamoDBClientConfig = {
   region: process.env.MY_AWS_REGION,
-};
-
-// 本番環境(Amplify)以外、つまりローカル環境の場合のみアクセスキーを使う
-if (process.env.NODE_ENV !== 'production') {
-  config.credentials = {
+  credentials: {
     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
-  };
-}
+  },
+};
+
+// // 本番環境(Amplify)以外、つまりローカル環境の場合のみアクセスキーを使う
+// if (process.env.NODE_ENV !== 'production') {
+//   config.credentials = {
+//     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID!,
+//     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
+//   };
+// }
 
 const client = new DynamoDBClient(config);
 export const docClient = DynamoDBDocumentClient.from(client);
