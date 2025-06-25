2f454838f6d744ecd2af5e26596d1b732fbc417c
TU <modptu@gmail.com>
2025-06-25 11:57:31 +0900

    Stabilize configuration with direct credentials

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -1,21 +1,12 @@
-import { DynamoDBClient, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const config: DynamoDBClientConfig = {
+const client = new DynamoDBClient({
   region: process.env.MY_AWS_REGION,
   credentials: {
     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
   },
-};
+});
 
-// // 本番環境(Amplify)以外、つまりローカル環境の場合のみアクセスキーを使う
-// if (process.env.NODE_ENV !== 'production') {
-//   config.credentials = {
-//     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID!,
-//     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
-//   };
-// }
-
-const client = new DynamoDBClient(config);
 export const docClient = DynamoDBDocumentClient.from(client);
